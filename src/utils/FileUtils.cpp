cd7899dca8f600e5c00242543b0bd0859c6a3352
kartikgarg1508 <kartikgarg394@gmail.com>
2024-03-27 00:13:32 +0530

    fix: tracefiles can be large, so reading only 1 million instructions

--- src/utils/FileUtils.cpp
+++ src/utils/FileUtils.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#define MAX_LINES 1000000
+
 std::string FileUtils::readFile(const std::string& filePath) {
 
     std::ifstream file(filePath);
@@ -11,11 +13,16 @@ std::string FileUtils::readFile(const std::string& filePath) {
 
     std::stringstream buffer;
     std::string line;
+    size_t lineCount = 0;
 
     while (std::getline(file, line)) {
         if (!line.empty()) {
             buffer << line << '\n';
+            ++lineCount;
         }
+
+        if (lineCount >= MAX_LINES)
+            break;
     }
 
     return buffer.str();
